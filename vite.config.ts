42a827a2dc0ed4b1f656043790188f0180a22044
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2024-09-03 15:07:42 +0700

    fix: renew esnext

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  }
 })
